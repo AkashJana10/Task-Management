80e54e5b631c68d5beb9c893e806375b44ce037d
AkashJana10 <akashjana9679@gmail.com>
2026-01-14 22:47:44 +0530

    production ready-1

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.MODE === "development" ?  "http://localhost:8080/api" : "/api"
 const axiosClient = axios.create({
-  baseURL: "http://localhost:8080/api" || "http://localhost:8000/api",
+  baseURL: BASE_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
